caed722893b715950f02dce30fbc16df05a99e98
Ericka <erickabenavidez236@gmail.com>
2026-09-18 10:33:47 +0800

    Add Admin Dashboard changing Program.cs (#7)

--- UI/Program.cs
+++ UI/Program.cs
@@ -11,12 +11,7 @@ namespace UI
             // To customize application configuration such as set high DPI settings or default font,
             // see https://aka.ms/applicationconfiguration.
             ApplicationConfiguration.Initialize();
-<<<<<<< Updated upstream
             Application.Run(new Login());
-=======
-            Application.Run(new AdminDashboard());
-
->>>>>>> Stashed changes
         }
     }
 }
